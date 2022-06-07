95e966542d6c92518198079357f0d97c5e00c5e7
Elivan-Jr <elivanlimajunior@gmail.com>
2022-06-07 19:20:57 -0300

    Adicionando Class ao span
    Adicionamos a Class span-active dinamicamente no nosso HTML.
    
    Após isso fomos no nosso código Style apenas por uma transição no nosso span, para o movimento na página ficar mas suave.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const inputs = document.querySelectorAll('.input'); // querySelectorAll por que será + de um input que iremos pegar.
 
-const handleFocus = (event) => {
-  console.log(event.target); // esse evento nós conseguimos acessar o target e ver o evento que disparou a ação. Veremos que ele printo o nosso input.
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling //Acessando o elemento anterior ao nosso target inicial que é o input. Assim chegamos no span.
+  span.classList.add('span-active') // adicionando a class a tag span
 }
 
 
@@ -10,11 +11,3 @@ const handleFocus = (event) => {
 
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus)) 
-
-// Adicionando Class no nosso span dinamicamente.
-// const handleFocus = ({ target }) => {
-//     const span = target.previousElementSibling;
-//     span.classList.add('span-active');
-//   }
-
-// inputs.forEach((input) => input.addEventListener('focus', handleFocus));
