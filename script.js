5de54434371f0f04df53d3d8274ffe3cf1babc48
Elivan-Jr <elivanlimajunior@gmail.com>
2022-06-07 19:25:02 -0300

    Removendo a class

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const inputs = document.querySelectorAll('.input'); // querySelectorAll por que será + de um input que iremos pegar.
 
+//adicionando a class
 const handleFocus = ({ target }) => {
   const span = target.previousElementSibling //Acessando o elemento anterior ao nosso target inicial que é o input. Assim chegamos no span.
   span.classList.add('span-active') // adicionando a class a tag span
+}
+
+// removendo a class
+
+const handleFocusOut = ({ target }) => {
+  const span = target.previousElementSibling
+  span.classList.remove('span-active')  //removendo a class da nossa tag span
 }
 
 
@@ -10,4 +18,6 @@ const handleFocus = ({ target }) => {
 //inputs[0].addEventListener('click', () => console.log('testando'))
 
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocus)) 
+inputs.forEach((input) => input.addEventListener('focus', handleFocus)) //escutador de eventos (Foi usado para adicionarmos um evento no input).
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocusOut)) //fizemos essa mesma estrutura para trabalharmos na remoção do span.
