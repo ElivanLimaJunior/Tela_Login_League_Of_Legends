881c34c430b4fdf569da205220282bc458a7ebbd
Elivan-Jr <elivanlimajunior@gmail.com>
2022-06-07 19:13:09 -0300

    Acessando o DOM
    Acessando input dentro do nosso código HTML

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const inputs = document.querySelectorAll('.input');
+const inputs = document.querySelectorAll('.input'); // querySelectorAll por que será + de um input que iremos pegar.
 
-const handleFocus = ({ target }) => {
-    const span = target.previousElementSibling;
-    span.classList.add('span-active');
-  }
+const handleFocus = (event) => {
+  console.log(event.target); // esse evento nós conseguimos acessar o target e ver o evento que disparou a ação. Veremos que ele printo o nosso input.
+}
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+
+//poderiamos fazer desse jeito, + é uma forma esquisita de fazer.
+//inputs[0].addEventListener('click', () => console.log('testando'))
+
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus)) 
+
+// Adicionando Class no nosso span dinamicamente.
+// const handleFocus = ({ target }) => {
+//     const span = target.previousElementSibling;
+//     span.classList.add('span-active');
+//   }
+
+// inputs.forEach((input) => input.addEventListener('focus', handleFocus));
