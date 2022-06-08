c3d1f55df244dd72b806b569ce5eaf1f3d087df8
Elivan-Jr <elivanlimajunior@gmail.com>
2022-06-08 19:41:12 -0300

    Style do SVG e código JavaScript
    Estilizamos a nossa imagem SVG e fizemos um código no JavaScript para remover a class "disabled" dinamicamente do HTML com uma condição.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll('.input'); // querySelectorAll por que será + de um input que iremos pegar.
+const button = document.querySelector('.login__button'); // acessando o botão
 
 //adicionando a class
 
@@ -15,12 +16,32 @@ const handleFocusOut = ({ target }) => {
     span.classList.remove('span-active');  //removendo a class da nossa tag span
   }
 }
-
-
 //poderiamos fazer desse jeito, + é uma forma esquisita de fazer.
 //inputs[0].addEventListener('click', () => console.log('testando'))
 
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocus)); //escutador de eventos (Foi usado para adicionarmos um evento no input).
+
+const handleChange = () => {
+  const [username, password] = inputs; // array destructor
+  // escrever dessa forma seria o mesmo que declarar 2 variaveis = input[0] e input[1]
+
+  // Condição para o desabled
+  if (username.value && password.value.length >= 8) {
+    button.removeAttribute('disabled');
+  } else {
+    button.setAttribute('disabled', '');
+  }
+ // aqui passamos 2 parametros, estamos mudando o "disabled" por um valor vazio. Por exemplo, pode ser usado em uma imagem.
+// ('disabled', 'teste');  Ficaria assim no HTMl: disabled="teste"
+}
+
+
+// Eventos
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus)); 
+//escutador de eventos (Foi usado para adicionarmos um evento no input).
 
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut)); //fizemos essa mesma estrutura para trabalharmos na remoção do span.
+
+inputs.forEach((input) => input.addEventListener('input', handleChange));
+// 
