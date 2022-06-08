da3d73bf01e6faec35e3cf12fc1c6d1e492644a4
Elivan-Jr <elivanlimajunior@gmail.com>
2022-06-07 23:06:15 -0300

    Estilizando Botões e Icones

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const inputs = document.querySelectorAll('.input'); // querySelectorAll por que será + de um input que iremos pegar.
 
 //adicionando a class
+
 const handleFocus = ({ target }) => {
-  const span = target.previousElementSibling //Acessando o elemento anterior ao nosso target inicial que é o input. Assim chegamos no span.
-  span.classList.add('span-active') // adicionando a class a tag span
+  const span = target.previousElementSibling; //Acessando o elemento anterior ao nosso target inicial que é o input. Assim chegamos no span.
+  span.classList.add('span-active'); // adicionando a class a tag span
 }
 
 // removendo a class
 
 const handleFocusOut = ({ target }) => {
-  const span = target.previousElementSibling
-  span.classList.remove('span-active')  //removendo a class da nossa tag span
+  if (target.value === ''){  
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');  //removendo a class da nossa tag span
+  }
 }
 
 
@@ -18,6 +21,6 @@ const handleFocusOut = ({ target }) => {
 //inputs[0].addEventListener('click', () => console.log('testando'))
 
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocus)) //escutador de eventos (Foi usado para adicionarmos um evento no input).
+inputs.forEach((input) => input.addEventListener('focus', handleFocus)); //escutador de eventos (Foi usado para adicionarmos um evento no input).
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocusOut)) //fizemos essa mesma estrutura para trabalharmos na remoção do span.
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut)); //fizemos essa mesma estrutura para trabalharmos na remoção do span.
